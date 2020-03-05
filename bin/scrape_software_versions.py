5fd0f08bb26e4e2f90121c9c34a58ec9dc833dda
Zethson <lukas.heumos@posteo.net>
2020-03-05 11:46:29 +0100

    [FIX] openms capture regex

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -3,12 +3,28 @@ from __future__ import print_function
 from collections import OrderedDict
 import re
 
-openms_version_regex = r"[0-9][.][0-9][.][0-9]"
+openms_version_regex = r"([0-9][.][0-9][.][0-9])"
 
 regexes = {
     'nf-core/proteomicslfq': ['v_pipeline.txt', r"(\S+)"],
     'Nextflow': ['v_nextflow.txt', r"(\S+)"],
     'ThermorawfileParser': ['v_thermorawfileparser.txt', r"(\S+)"],
+    'FileConverter': ['v_fileconverter.txt', openms_version_regex],
+    'DecoyDatabase': ['v_decoydatabase.txt', openms_version_regex],
+    'MSGFPlusAdapter': ['v_msgfplusadapter.txt', openms_version_regex],
+    'MSGFPlus': ['v_msgfplus.txt', r"\(([^)]+)\)"],
+    'CometAdapter': ['v_cometadapter.txt', openms_version_regex],
+    'Comet': ['v_comet.txt', r"\"(.*)\""],
+    'PeptideIndexer': ['v_peptideindexer.txt', openms_version_regex],
+    'PSMFeatureExtractor': ['v_psmfeatureextractor.txt', openms_version_regex],
+    'PercolatorAdapter': ['v_percolatoradapter.txt', openms_version_regex],
+    'Percolator': ['v_percolator.txt', r"([0-9].[0-9]{2}.[0-9])"],
+    'IDFilter': ['v_idfilter.txt', openms_version_regex],
+    'IDScoreSwitcher': ['v_idscoreswitcher.txt', openms_version_regex],
+    'FalseDiscoveryRate': ['v_falsediscoveryrate.txt', openms_version_regex],
+    'IDPosteriorErrorProbability': ['v_idposteriorerrorprobability.txt', openms_version_regex],
+    'ProteomicsLFQ': ['v_proteomicslfq.txt', openms_version_regex],
+    'MSstats': ['v_msstats_plfq.txt', r"(\S+)"]
 }
 results = OrderedDict()
 results['nf-core/proteomicslfq'] = '<span style="color:#999999;\">N/A</span>'
